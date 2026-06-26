e490ed6b9e6342b634c2292725d61924b40589a9
Marcos <mazoti@gmail.com>
2026-06-26 15:41:50 -0300

    main_linux.cpp fixed

--- src/main_linux.cpp
+++ src/main_linux.cpp
@@ -4,17 +4,17 @@
 import i18n;
 import core;
 
-int main(int argc, char* argv[], char* envp[]){
+int main(int argc, const char* argv[], const char* envp[]) {
 	// Use a UTF-8 locale
 	std::setlocale(LC_ALL, "");
 
 	// System header with version
-	std::wcout << std::endl << HEADER << std::endl << std::endl;
+	std::wcout << std::endl << i18n::HEADER << std::endl << std::endl;
 
 	run(envp, ':');
 
 	if(argc == 1){
-		std::wcout << ANY_KEY_TO_CLOSE << std::endl;
+		std::wcout << i18n::ANY_KEY_TO_CLOSE << std::endl;
 		getchar();
 	}
 
